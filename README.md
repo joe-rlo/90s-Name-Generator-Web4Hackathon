59a2ddc7f419afcb7694e4888dcca76a25964ccc
Joe - RLO <joe@rlopodcast.com>
2022-07-03 04:50:55 -0400

    wicked cool first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+90s Wallet Name Generator
+==========================
+
+A totally rad site that let's you generate a killer name. You can then check if it already exists before trying creating it or whatever.
+
+Just don't have a cow if its already taken.
+
+Party on dawg!
+
+
+
+Uses:
+
+* [Web4](https://web4.near.page) is a gateway to the smart contracts deployed to NEAR protocol.
+* NEAR API JS
+* JQuery CLI (just to add some fun in the experience)
---
+++ src/web4.rs
@@ -0,0 +1,86 @@
+use crate::*;
+
+use near_sdk::json_types::Base64VecU8;
+use near_sdk::{env};
+use std::collections::HashMap;
+
+#[allow(dead_code)]
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Web4Request {
+    #[serde(rename = "accountId")]
+    account_id: Option<AccountId>,
+    path: String,
+    params: Option<HashMap<String, String>>,
+    query: Option<HashMap<String, Vec<String>>>,
+    preloads: Option<HashMap<String, Web4Response>>,
+}
+
+#[derive(Serialize, Deserialize, Default)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Web4Response {
+    #[serde(rename = "contentType")]
+    content_type: Option<String>,
+    status: Option<u32>,
+    body: Option<Base64VecU8>,
+    #[serde(rename = "bodyUrl")]
+    body_url: Option<String>,
+    #[serde(rename = "preloadUrls")]
+    preload_urls: Option<Vec<String>>,
+}
+
+impl Web4Response {
+    pub fn html_response(text: String) -> Self {
+        Self {
+            content_type: Some(String::from("text/html; charset=UTF-8")),
+            body: Some(text.into_bytes().into()),
+            ..Default::default()
+        }
+    }
+
+    pub fn plain_response(text: String) -> Self {
+        Self {
+            content_type: Some(String::from("text/plain; charset=UTF-8")),
+            body: Some(text.into_bytes().into()),
+            ..Default::default()
+        }
+    }
+
+    pub fn preload_urls(urls: Vec<String>) -> Self {
+        Self {
+            preload_urls: Some(urls),
+            ..Default::default()
+        }
+    }
+
+    pub fn body_url(url: String) -> Self {
+        Self {
+            body_url: Some(url),
+            ..Default::default()
+        }
+    }
+
+    pub fn status(status: u32) -> Self {
+        Self {
+            status: Some(status),
+            ..Default::default()
+        }
+    }
+}
+
+#[near_bindgen]
+impl Contract {
+    #[allow(unused_variables)]
+    pub fn web4_get(&self, request: Web4Request) -> Web4Response {
+        let path = request.path;
+
+        if path == "/robots.txt" {
+            return Web4Response::plain_response("User-agent: *\nDisallow:".to_string());
+        }
+
+        Web4Response::html_response(
+            include_str!("../res/index.html").to_string()
+        )
+
+    }
+}
